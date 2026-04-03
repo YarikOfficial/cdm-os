42374e4679712c82ee9059a536b971f0ce8c3c39
Ярик <yarik.official@ya.ru>
2026-04-04 03:19:40 +0700

    Терминал, r/w регистры, код прерывания

--- src/main.asm
+++ src/main.asm
@@ -21,11 +21,25 @@ default_handler>
     halt
 
 key_handler>
-    ldi r0, 0xF000 # Символ на клавиатуре и статус
-    ldw r0, r1
-    ldi r0, 5
-    add r0, r1
+    save r0
+    save r1
+    save r2
 
+    ldi r0, 0xF000 # Символ на клавиатуре
+    ldi r1, 0xF008 # Вход терминала
+
+    ldw r0, r0 # Загружаем символ из клавиатуры
+    stw r1, r0 # Выводим на терминал
+
+    ldi r0, 0xF002 # Регистр состояния клавиатуры
+    ldi r2, 0x1
+    ldw r0, r1
+    or r2, r1
+    stw r0, r1
+
+    restore r0
+    restore r1
+    restore r2
     rti
 
 
@@ -39,6 +53,10 @@ prog2: ext
 core_init>
     ldi r0, 0x8000 # Меняем указатель стека
     stsp r0
+
+    ldi r0, 0xF00A # Регистр терминала
+    ldi r1, 0x1 # Включение вывода терминала
+    stw r0, r1
 
     ei # enable interrupts
 loop:
