423d531fc5ab7c7843e70cbceda88e4900a486fc
Ярик <yarik.official@ya.ru>
2026-04-04 01:34:43 +0700

    клавиатура + прерывания - тупейший баг с r/w и read

--- src/main.asm
+++ src/main.asm
@@ -1,24 +1,50 @@
+### INTERRUPT TABLE ###
 asect 0x00
-monitor: ext
+core_init: ext
 default_handler: ext
+key_handler: ext
 
-dc monitor, 0
-dc default_handler, 0
-dc default_handler, 0
-dc default_handler, 0
-dc default_handler, 0
-align 0x80
+# Таблица векторных прерываний (IVT)
+dc core_init, 0 # Вектор запуска/сброса, начальный PS
+dc default_handler, 0 # Невыровненный SP
+dc default_handler, 0 # Невыровненный PC
+dc default_handler, 0 # Недопустимая инструкция
+dc default_handler, 0 # Double Fault
+dc key_handler, 0 # Прерывания клавиатуры
+align 0x80 # Зарезервировать место для других IVT
 
+
+### HANDLERS ###
 rsect exc_handlers
+
 default_handler>
     halt
 
-rsect monitor
+key_handler>
+    ldi r0, 0xF000 # Символ на клавиатуре и статус
+    ldw r0, r1
+    ldi r0, 5
+    add r0, r1
+
+    rti
+
+
+### CORE ###
+rsect os_core
+
 prog1: ext
 prog2: ext
-monitor>
-    #ldi r0, 0x0100
-    #ldw r0, r7
-    #jsr prog1
-    br prog1
+
+# Code
+core_init>
+    ldi r0, 0x8000
+    stsp r0
+
+    ei # enable interrupts
+loop:
+    wait # wait for an interrupt
+    #int 5
+    br loop # loop forever ;)
+    di # disable interrupts
+    
 end.
