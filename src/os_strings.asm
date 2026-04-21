a7ebfe4c096e9515d439c0e7defac5b5c76cc7ed
Ярик <yarik.official@ya.ru>
2026-04-21 10:09:39 +0700

    manually merge os_strings

--- src/os_strings.asm
+++ src/os_strings.asm
@@ -1,9 +1,11 @@
-rsect OS_STRINGS
+rsect os_strings
 
-### Strings for output
 os_string_text> dc "This is a test text!\n", 0
 os_string_greeting> dc "Welcome to ZachetOS!\n", 0
-os_string_help> dc "Available commands: None ;)\n", 0
+os_string_help> dc "Available commands: help, ls, prog1, prog2\n", 0
+os_string_ls> dc "prog1\nprog2\n", 0
+os_string_prog1> dc "Program 1 started!\n", 0
+os_string_prog2> dc "Program 2 started!\n", 0
 os_string_error_invalid_command> dc "This command is INVALID!\n", 0
 os_string_prompt_start> dc ">", 0
     
