6432d50b3716cd7834cbc2d1afc19332fab1c3f9
Ярик <yarik.official@ya.ru>
2026-04-21 10:07:53 +0700

    manually merge prog_table

--- src/programs.asm
+++ src/programs.asm
@@ -1,19 +1,25 @@
 asect 0x8000
+
+os_string_prog1: ext
+os_string_prog2: ext
+kernel_driver_tty_print: ext
+
+file_table>
+    dc file_name_prog1, prog1
+    dc file_name_prog2, prog2
+    dc 0, 0
+
+file_name_prog1> dc "prog1", 0
+file_name_prog2> dc "prog2", 0
+
 prog1>
-    ldi r1, 10
-    ldi r2, 20
-    add r1, r2, r3
-    ldi r7, after_prog
-    jsr r7
+    ldi r0, os_string_prog1
+    jsr kernel_driver_tty_print
+    rts
 
 prog2>
-    ldi r4, 7
-    ldi r5, 8
-    add r4, r5, r6
-    ldi r7, after_prog
-    jsr r7
-
-after_prog>
-    halt
+    ldi r0, os_string_prog2
+    jsr kernel_driver_tty_print
+    rts
 
 end
